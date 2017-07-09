eadac7d4e0e7a3a4fbe0a2acc93d8bb0ef102eae
jc2729 <jc2729@cornell.edu>
2017-07-08 23:32:21 -0700

    just messing around with iOS app basic drawing functionality

--- Project/ViewController.swift
+++ Project/ViewController.swift
@@ -9,16 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .white
+        
+        //modify to interact with storyboard
+//        let newProjectButton = UIButton(frame: CGRect(x: 0, y: 0, width: 300, height: 30))
+//        newProjectButton.setTitle("New Project", for: UIControlState.normal)
+//        newProjectButton.center = view.center
+//        newProjectButton.setTitleColor(.blue, for: .normal)
+//        newProjectButton.addTarget(self, action: #selector(startNewProject), for: .touchUpInside)
+//        
+//        view.addSubview(newProjectButton)
+        
+    }
+    
+    func startNewProject() {
+//        //api call here to generate code
+//        //push new controller, pass code to controller via delegate
+//        let code = 000000
+//        let sketchpadViewController = SketchpadViewController()
+//        sketchpadViewController.code = code
+//        navigationController?.pushViewController(sketchpadViewController, animated: true)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
