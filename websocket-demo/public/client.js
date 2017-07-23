e5896e7cafad4731560d4c2f3c7e8b6497305710
Kenneth Yuan <kenny.k.yuan@gmail.com>
2017-07-23 16:54:13 -0700

    Fixed up the front-end (splash page and canvas page). Added an access code entry field (which needs to be connected to the database later) as well as a close and download button on the canvas page. Created a draw method to convert the data stored in the Firebase db to actual strokes on the canvas. Should be more or less ready to add in backend functionality!

--- websocket-demo/public/client.js
+++ websocket-demo/public/client.js
@@ -3,43 +3,31 @@ document.getElementById('connect').addEventListener('click', function(event) {
   var socket = new WebSocket('ws://localhost:8081/');
 
   socket.onopen = function(event) {
-    log('Opened connection 🎉');
+    console.log('Opened connection 🎉');
 
     var accessCode = document.getElementById('access').value;
 
-    var json = JSON.stringify({ message: accessCode });
+    var json = JSON.stringify({ code: accessCode });
     socket.send(json); //websocket has a send method for pushing data to the server and you can provide an onmessage handler for receiving data from the serve
-    log('Sent: ' + json);
+    console.log('Sent: ' + json);
   }
 
   socket.onerror = function(event) { //connects to the  socket.on('message', function(message) {}); 
-    log('Error: ' + JSON.stringify(event));
+    console.log('Error: ' + JSON.stringify(event));
   }
 
   socket.onmessage = function (event) {
-    log('Received: ' + event.data);
+    console.log('Received: ' + event.data);
   }
 
   socket.onclose = function(event) {
-    log('Closed connection 😱');
+    console.log('Closed connection 😱');
   }
 
   document.querySelector('#close').addEventListener('click', function(event) {
     socket.close();
-    log('Closed connection 😱');
+    console.log('Closed connection 😱');
   });
-
-  document.querySelector('#send').addEventListener('click', function(event) {
-    var json = JSON.stringify({ message: 'Hey there' });
-    socket.send(json);
-    log('Sent: ' + json);
-  });
-
-  var log = function(text) {
-    var li = document.createElement('li');
-    li.innerHTML = text;
-    document.getElementById('log').appendChild(li);
-  }
 
   window.addEventListener('beforeunload', function() {
     socket.close();
